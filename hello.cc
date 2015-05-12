63668dbdc7452b54530ec644465d1937dbe9c0c1
jlchen <jianglu.chen@gmail.com>
2015-05-12 12:32:29 +0800

    add hello using c++ 11 compiler

---
+++ hello.cc
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cc
+ *
+ *    Description:  test if programs compilered by c++ 11 can be run by c++ 97
+ *
+ *        Version:  1.0
+ *        Created:  05/12/2015 12:17:06 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<< "hello world"<< endl;
+    return 0;
+}
